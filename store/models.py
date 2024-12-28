e2ca571b5c685ff5e4d7237bc4ef1c2e1f048dcc
aigerim678 <aigerimsabyrkhan393@gmail.com>
2024-12-28 20:17:10 +0500

    Like, Bookmarks, Rating

--- store/models.py
+++ store/models.py
@@ -6,7 +6,27 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')
 
     def __str__(self):
         return f'Id{self.id}: {self.name}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'OK'),
+        (2, 'FINE'),
+        (3, 'GOOD'),
+        (4, 'AMAZING'),
+        (5, 'INCREDIBLE')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveIntegerField(choices=RATE_CHOICES, null=True)
+
+
+    def __str__(self):
+        return f'{self.user.username}: {self.book.name}, {self.rate}'
--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.utils import json
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 from store.serializers import BooksSerializer
 
 
@@ -125,3 +125,46 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
 
 
+class BooksRelationTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='test_username')
+        self.user2 = User.objects.create(username='test_username2')
+        self.book_1 = Book.objects.create(name='Test book 1', price=25, author='Author 1', owner = self.user)
+        self.book_2 = Book.objects.create(name='Test book 2', price=55, author='Author 4', owner = self.user)
+        self.book_3 = Book.objects.create(name='Test book Author 1', price=75, author='Author 2', owner = self.user)
+
+
+
+    def test_like(self):
+        url = reverse('userbookrelation-detail', args=(self.book_1.id, ))
+        data = {
+            "like": True
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        relation = UserBookRelation.objects.get(user=self.user, book=self.book_1)
+        self.assertTrue(relation.like)
+
+        data = {
+            "in_bookmarks": True
+        }
+        json_data = json.dumps(data)
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        relation = UserBookRelation.objects.get(user=self.user, book=self.book_1)
+        self.assertTrue(relation.in_bookmarks)
+
+
+    def test_rate(self):
+        url = reverse('userbookrelation-detail', args=(self.book_1.id, ))
+        data = {
+            "rate": 5
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        relation = UserBookRelation.objects.get(user=self.user, book=self.book_1)
+        self.assertEqual(5, relation.rate)
